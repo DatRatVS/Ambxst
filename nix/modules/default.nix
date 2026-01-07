a917cea61d98a4ab4006e87cbf250b2073a1b43e
Axenide <axenide@gmail.com>
2026-01-07 16:38:53 -0300

    nix fix

--- nix/modules/default.nix
+++ nix/modules/default.nix
@@ -1,8 +1,23 @@
 # NixOS module for Ambxst
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
-{
-  config = lib.mkIf (!config.networking.networkmanager.enable) {
+let
+  cfg = config.programs.ambxst;
+in {
+  options.programs.ambxst = {
+    enable = lib.mkEnableOption "Ambxst shell";
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      description = "The Ambxst package to use";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [ cfg.package ];
+
+    # Enable recommended services for full functionality
+    services.power-profiles-daemon.enable = lib.mkDefault true;
     networking.networkmanager.enable = lib.mkDefault true;
   };
 }
